12c37a137713808e712ef7e62c632d3aa21f35ff
carotis_interna <interna.carotis@gmail.com>
2020-11-30 22:43:02 +0300

    #1 Реализовать сплеш экран

---
+++ app/src/main/java/ru/popova/memes/SplashActivity.kt
@@ -0,0 +1,25 @@
+package ru.popova.memes
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed(
+            {
+                val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            },
+            3000
+        )
+    }
+}
