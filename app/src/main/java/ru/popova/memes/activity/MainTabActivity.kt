420e1a7b22d1076d810ad6930eaa349038597e43
carotis_interna <interna.carotis@gmail.com>
2020-12-01 21:19:05 +0300

    A6: Верстка списка мемов

--- app/src/main/java/ru/popova/memes/activity/MainTabActivity.kt
+++ app/src/main/java/ru/popova/memes/activity/MainTabActivity.kt
@@ -2,12 +2,36 @@ package ru.popova.memes.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.popova.memes.R
+import ru.popova.memes.activity.fragment.MemeListFragment
+import ru.popova.memes.activity.fragment.NewMemeFragment
+import ru.popova.memes.activity.fragment.ProfileFragment
 
 class MainTabActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_tab)
-        supportActionBar?.hide()
+        supportActionBar?.hide();
+        openFragment(MemeListFragment())
+
+        val navbar: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        navbar.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.action_meme_list -> MemeListFragment()
+                R.id.action_new_meme -> NewMemeFragment()
+                R.id.action_profile -> ProfileFragment()
+                else -> null
+            }?.let { fragment ->
+                openFragment(fragment)
+            }
+
+            true
+        }
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.tab_frame, fragment).commit()
     }
 }
---
+++ app/src/main/java/ru/popova/memes/adapter/MemeListAdapter.kt
@@ -0,0 +1,44 @@
+package ru.popova.memes.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import ru.popova.memes.R
+import ru.popova.memes.dto.MemeDto
+
+class MemeViewHolder(
+    itemView: View,
+    val memeImage: ImageView = itemView.findViewById(R.id.meme_image),
+    val title: TextView = itemView.findViewById(R.id.meme_title),
+    val favButton: ImageButton = itemView.findViewById(R.id.favButton),
+    val shareButton: ImageButton = itemView.findViewById(R.id.shareButton)
+) : RecyclerView.ViewHolder(itemView)
+
+
+class MemeListAdapter(
+    private val ctx: Context,
+    private val list: List<MemeDto>
+) :
+    RecyclerView.Adapter<MemeViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemeViewHolder {
+        return MemeViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.cell_meme, parent, false)!!
+        )
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    override fun onBindViewHolder(holder: MemeViewHolder, position: Int) {
+        val value = list[position]
+        Glide.with(ctx).load(value.photoUrl).into(holder.memeImage)
+        holder.title.text = value.title
+        val imageResource = if (value.isFavorite) R.drawable.fav_selected else R.drawable.fav
+        holder.favButton.setImageResource(imageResource)
+    }
+}
