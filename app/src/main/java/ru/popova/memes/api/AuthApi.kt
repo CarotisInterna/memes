72f8678dd00ac48a1b4c1b6875d3b5021aaefbb5
carotis_interna <interna.carotis@gmail.com>
2020-12-01 14:15:05 +0300

    #2 Инициализация экрана авторизации и базовая логика #3 Реализовать метод для выполнения запроса авторизации пользователя

---
+++ app/src/main/java/ru/popova/memes/api/AuthApi.kt
@@ -0,0 +1,15 @@
+package ru.popova.memes.api
+
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+import ru.popova.memes.dto.AuthInfo
+import ru.popova.memes.dto.LoginRequestDto
+
+interface AuthApi {
+    @POST("auth/login")
+    fun login(@Body loginRequest: LoginRequestDto): Call<AuthInfo?>
+
+    @POST("auth/logout")
+    fun logout(): Any
+}
